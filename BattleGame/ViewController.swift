de44db65f4a62beaff8a80a782275415e2452e81
mikepitre <michaelepitre@gmail.com>
2015-10-23 12:56:51 -0500

    added outlets and actions to view controller

--- BattleGame/ViewController.swift
+++ BattleGame/ViewController.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //variables
+    var player1 = Character(name: "Player 1")
+    var player2 = Character(name: "Player 2")
+    
+    //Outlets
+    @IBOutlet weak var player1Image: UIImageView!
+    @IBOutlet weak var player2Image: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func player1AttackBtn(sender: AnyObject) {
+    }
+    
+    @IBAction func player2AttackBtn(sender: AnyObject) {
+    }
+    
 
 
 }
